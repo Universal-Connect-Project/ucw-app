8bdea67b45d9306c2dc065fe557823fcb33caada
Tyson Phalp <tyson.phalp@mx.com>
2024-03-13 09:21:17 -0600

    Add ui with npm widget component

---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+import { ConnectWidget } from "@ucp-npm/components";
+import "./App.css";
+
+/**
+ * Renders the main App component.
+ *
+ * @return {React.ReactElement} The rendered main App component
+ */
+function App(): ReactElement {
+  return <ConnectWidget />;
+}
+
+export default App;
---
+++ ui/src/main.tsx
@@ -0,0 +1,12 @@
+import "./init.ts";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
