299b95b6f8fd60af719633afdf1ba3b56100537a
Wes Risenmay <wes.risenmay@mx.com>
2024-03-26 15:05:40 -0600

    changing the path

--- cypress/e2e/mxHappyPath.cy.ts
+++ cypress/e2e/mxHappyPath.cy.ts
@@ -2,10 +2,11 @@ describe('Should connect to an Institution through happy path', () => {
     before(() => {
       cy.setAuthCode()
     })
-    
+
     it('Connects to MX Bank', () => {
       const authCode = Cypress.env("authCode")
-      cy.visit(`http://localhost:8080/?job_type=agg&auth=${authCode}`)
+      
+      cy.visit(`http://localhost:5173/?job_type=agg&auth=${authCode}`)
       cy.get('#mx-connect-search').type('MX Bank')
       cy.get(`button[aria-label*="Add account with MX Bank"]`).first().click()
       cy.get('#LOGIN').type('mxuser')
