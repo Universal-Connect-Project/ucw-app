d21d3619304f62bffba2de80edb29fe006a5b556
Wes Risenmay <wes.risenmay@mx.com>
2024-04-08 07:41:05 -0600

    added a test case for mx oauth

--- cypress/e2e/mx.cy.ts
+++ cypress/e2e/mx.cy.ts
@@ -6,7 +6,6 @@ describe('Should connect to an Institution through happy path', () => {
   it('Connects to MX Bank', () => {
     const authCode = Cypress.env('authCode')
 
-    // cy.visit(`http://localhost:8080/?job_type=agg&auth=${authCode}`)
     cy.visit(`http://localhost:8080/?job_type=agg&auth=${authCode}`)
     cy.findByPlaceholderText('Search').type('MX Bank')
     cy.findByLabelText('Add account with MX Bank').first().click()
@@ -15,5 +14,15 @@ describe('Should connect to an Institution through happy path', () => {
     cy.findByRole('button', { name: 'Continue' }).click()
 
     cy.findByText('Connected', { timeout: 45000 }).should('exist')
+  })
+
+  it('Connects to MX Bank (OAuth)', () => {
+    const authCode = Cypress.env('authCode')
+
+    cy.visit(`http://localhost:8080/?job_type=agg&auth=${authCode}`)
+    cy.findByPlaceholderText('Search').type('MX Bank (Oauth)')
+    cy.findByLabelText('Add account with MX Bank (Oauth)').first().click()
+
+    cy.findByRole('link', { name: 'Continue' }).should('exist')
   })
 })
