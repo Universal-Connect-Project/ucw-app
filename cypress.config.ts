0dfe131fb7684406aec47509c0fdeffb42cdb394
Craig Tingey <craig.tingey@mx.com>
2024-04-11 15:37:29 -0600

    add retry if test fails

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    retries: 1,
     setupNodeEvents (on, config) {
       // implement node event listeners here
     }
