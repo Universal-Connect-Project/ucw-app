8d9a784ad807067c098100ad380d51da08df80ca
Craig Tingey <craig.tingey@mx.com>
2024-04-11 15:44:46 -0600

    increase default timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     retries: 1,
+    defaultCommandTimeout: 8000,
     setupNodeEvents (on, config) {
       // implement node event listeners here
     }
