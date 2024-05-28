1bd159a191d677fb01b1b0090cb0ebc3a683afa0
Wes Risenmay <wes.risenmay@mx.com>
2024-05-28 14:57:00 -0600

    added cypress tests to check that login shows for popular institutions

--- apps/server/cypress/e2e/finicity.cy.ts
+++ apps/server/cypress/e2e/finicity.cy.ts
@@ -1,11 +1,11 @@
-describe("Should connect to an Institution through happy path", () => {
-  it("Connects to Finbank profiles a", () => {
-    const authCode = Cypress.env("authCode")
+describe('Should connect to an Institution through happy path', () => {
+  it('Connects to Finbank profiles a', () => {
+    const authCode = Cypress.env('authCode')
 
-    cy.visit(`http://localhost:8080/?job_type=agg`)
-    cy.findByPlaceholderText("Search").type("finbank profiles")
-    cy.findByLabelText("Add account with finbank profiles - a").click()
+    cy.visitAgg()
+    cy.findByPlaceholderText('Search').type('finbank profiles')
+    cy.findByLabelText('Add account with finbank profiles - a').click()
 
-    cy.findByRole("link", { name: "Continue" }).should("exist")
+    cy.findByRole('link', { name: 'Continue' }).should('exist')
   })
 })
