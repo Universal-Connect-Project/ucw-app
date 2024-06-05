0c24e8a480f05821ac8cbf32b776d22c781a932c
Wes Risenmay <wes.risenmay@mx.com>
2024-06-05 09:27:43 -0600

    added end to end tests to make sure preferences are working

--- apps/server/cypress/e2e/preferences.cy.ts
+++ apps/server/cypress/e2e/preferences.cy.ts
@@ -1,5 +1,27 @@
+// These tests expect local preferences to match testPreferences.json
 describe('preferences', () => {
-  it('uses local preferences to show favorite institutions', () => {})
+  it('uses local preferences to show favorite institutions', () => {
+    cy.visitAgg()
 
-  it('uses local preferences to hide institutions when searching', () => {})
+    const institutions = [
+      'Wells Fargo',
+      'Capital One',
+      'Bank of America',
+      'Citibank',
+      'TD Bank (US)',
+      'PNC Bank'
+    ]
+
+    institutions.forEach((institutionName) => {
+      cy.findByText(institutionName).should('exist')
+    })
+  })
+
+  it('uses local preferences to hide institutions when searching', () => {
+    cy.visitAgg()
+    cy.findByPlaceholderText('Search').type('Chase')
+
+    cy.findByText('Chase UCard').should('exist')
+    cy.findByText('Chase Bank').should('not.exist')
+  })
 })
