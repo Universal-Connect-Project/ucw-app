b397366963db00e4ddb88bd71dd8df4e62b200ac
Wes Risenmay <wes.risenmay@mx.com>
2024-06-06 11:56:24 -0600

    change command name from cypress to e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     }
   ],
   webServer: {
-    command: 'npm run dev:cypress',
+    command: 'npm run dev:e2e',
     url: 'http://127.0.0.1:8080',
     reuseExistingServer: !process.env.CI
   }
