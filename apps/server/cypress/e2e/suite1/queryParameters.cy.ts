0f4f695530674246f7f5f134223f02a7ac5df5c8
Wes Risenmay <wes.risenmay@mx.com>
2024-06-28 12:57:49 -0600

    improve documentation for institutionId and improve the test

--- apps/server/cypress/e2e/suite1/queryParameters.cy.ts
+++ apps/server/cypress/e2e/suite1/queryParameters.cy.ts
@@ -1,11 +1,14 @@
 const MX_BANK_INSTITUTION_ID = 'UCP-bb5296bd5aae5d9'
 
 describe('query parameters', () => {
-  it('skips straight to the institution if an institution_id is provided in the query parameters and completes the connection', () => {
+  it('skips straight to the institution if an institution_id is provided in the query parameters, hides the back button, and completes the connection', () => {
     cy.visit(`/?job_type=aggregate&institution_id=${MX_BANK_INSTITUTION_ID}`)
 
     cy.findByLabelText('LOGIN').type('mxuser')
     cy.findByLabelText('PASSWORD').type('correct')
+
+    cy.findByRole('button', { name: 'Back' }).should('not.exist')
+
     cy.findByRole('button', { name: 'Continue' }).click()
 
     cy.findByText('Connected', { timeout: 45000 }).should('exist')
