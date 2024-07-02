fbd8ab8ed9c8a46cfb439a1bc8c85467d8acef17
Wes Risenmay <wes.risenmay@mx.com>
2024-07-02 10:40:30 -0600

    added a test for the sophtron vc client

--- apps/server/src/providerApiClients/sophtronClient/vc.test.ts
+++ apps/server/src/providerApiClients/sophtronClient/vc.test.ts
@@ -1,10 +1,15 @@
+import { sophtronVcTranscationsData } from '../../test/testData/sophtronVcData'
 import providerCredentials from '../../providerCredentials'
 import SophtronVcClient from './vc'
 
 const vcClient = new SophtronVcClient(providerCredentials.sophtron)
 
 describe('Sophtron Vc Client', () => {
-  //   it('returns the data from a vc endpoint', async () => {
-  //     const response = await vcClient.getVC('')
-  //   })
+  it('returns the data from a vc endpoint', async () => {
+    const response = await vcClient.getVC(
+      'customers/userId/accounts/accountId/transactions'
+    )
+
+    expect(response).toEqual(sophtronVcTranscationsData)
+  })
 })
