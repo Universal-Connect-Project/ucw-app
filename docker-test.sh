7dca4879f161b05fae94f30fdd072380e806568c
Tyson Phalp <tyson.phalp@mx.com>
2024-09-17 12:13:04 -0600

    Fix check

---
+++ docker-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#docker compose rm -f -s
+#docker compose pull --ignore-buildable --quiet
+#docker compose up --build --wait-timeout 300 --detach
+
+BUILD_CHECK=$(docker logs ucw-app-server 2>&1 | grep "App initialized successfully")
+
+echo '---------------------------------'
+echo "${BUILD_CHECK}"
+echo '---------------------------------'
+
+if [[ $BUILD_CHECK == *"App initialized successfully"* ]]; then
+  echo 'Build passed!'
+else
+  echo 'Build failed! :('
+fi
