2f9e1049ee5f087d301521c46c7e5c7a4a031362
Tyson Phalp <tyson.phalp@mx.com>
2024-09-17 13:52:18 -0600

    See if it works with devDeps

--- apps/server/src/services/storageClient/redis.ts
+++ apps/server/src/services/storageClient/redis.ts
@@ -81,6 +81,7 @@ redisClient
     await setNoExpiration(PREFERENCES_REDIS_KEY, preferencesToSet || {})
   })
   .catch((reason) => {
-    error('Failed to connect to redis server: ' + JSON.stringify(reason))
+    error('Error details: ' + JSON.stringify(reason))
+    error('Failed to connect to redis server: ' + reason)
     info('No redis connection')
   })
