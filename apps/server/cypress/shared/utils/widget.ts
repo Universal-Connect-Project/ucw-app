0aa8efd2c43a1a03c08e825bc8c9d78081a7ef5c
Tyson Phalp <tyson.phalp@mx.com>
2024-10-02 15:20:39 -0600

    Remove some finicity stuff, since we don't currently support that agg (#121)

--- apps/server/cypress/shared/utils/widget.ts
+++ apps/server/cypress/shared/utils/widget.ts
@@ -1,15 +1,15 @@
 export const clickContinue = () => {
-  cy.findByRole('button', { name: 'Continue' }).click()
-}
+  cy.findByRole("button", { name: "Continue" }).click();
+};
 
 export const expectConnectionSuccess = () => {
-  cy.findByText('Connected', { timeout: 90000 }).should('exist')
-}
+  cy.findByText("Connected", { timeout: 240000 }).should("exist");
+};
 
 export const searchByText = (text) => {
-  cy.findByPlaceholderText('Search').type(text)
-}
+  cy.findByPlaceholderText("Search").type(text);
+};
 
 export const selectInstitutionByName = (name) => {
-  cy.findByLabelText(`Add account with ${name}`).first().click()
-}
+  cy.findByLabelText(`Add account with ${name}`).first().click();
+};
