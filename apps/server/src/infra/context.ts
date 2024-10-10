0092c115264e383bc15a333a98d1b21b5fbe9469
Tyson Phalp <tyson.phalp@mx.com>
2024-10-10 11:24:28 -0600

    Fixes for mx adapter package, and a couple minor improvements to other files

--- apps/server/src/infra/context.ts
+++ apps/server/src/infra/context.ts
@@ -1,15 +1,13 @@
-import type { NextFunction, Request, Response } from "express"
-import config from "../config"
-import { encrypt, decrypt } from "../utils"
-import { Context } from "../shared/contract"
+import type { NextFunction, Request, Response } from "express";
+import type { Context } from "../shared/contract";
 
 declare global {
   namespace Express {
     interface Request {
-      context?: Context
+      context?: Context;
     }
     interface Response {
-      context?: Context
+      context?: Context;
     }
   }
 }
@@ -17,27 +15,29 @@ declare global {
 export function contextHandler(
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) {
-  let context = {} as Context
+  let context = {} as Context;
   if (req.headers.meta?.length > 0) {
-    context = JSON.parse(req.headers.meta as string)
-    context.updated = false
+    context = JSON.parse(req.headers.meta as string);
+    context.updated = false;
   }
 
-  res.context = context
-  req.context = context
+  res.context = context;
+  req.context = context;
 
-  const { send } = res
+  const { send } = res;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   res.send = function (...args: any): any {
-    res.send = send
+    res.send = send;
 
     if (res.context.updated) {
-      res.set("meta", JSON.stringify(res.context))
+      res.set("meta", JSON.stringify(res.context));
     }
 
-    send.apply(res, args)
-  }
+    send.apply(res, args);
+  };
 
-  next()
+  next();
 }
