18ddaed2ecb16d75de88db82cbc39fa6be448f94
Tyson Phalp <tyson.phalp@mx.com>
2024-11-03 23:50:57 -0700

    Add mx-adapter until 1.0.0

--- apps/server/src/adapterSetup.ts
+++ apps/server/src/adapterSetup.ts
@@ -1,3 +1,8 @@
+import { getMxAdapterMapObject } from "@ucp-npm/mx-adapter";
+
+import config from "./config";
+import { get, set } from "./services/storageClient/redis";
+import * as logger from "./infra/logger";
 import { SophtronAdapter } from "./adapters/sophtron";
 
 import getSophtronVc from "./services/vcAggregators/sophtronVc";
@@ -10,8 +15,38 @@ const sophtronAdapterMapObject = {
   },
 };
 
+const mxAdapterMapObject = getMxAdapterMapObject({
+  cacheClient: {
+    set: set,
+    get: get,
+  },
+  logClient: logger,
+  aggregatorCredentials: {
+    mxInt: {
+      username: config.MxClientId,
+      password: config.MxApiSecret,
+      basePath: "https://int-api.mx.com",
+      vcEndpoint: "https://int-api.mx.com/",
+      aggregator: "mx_int",
+      available: true,
+    },
+    mxProd: {
+      username: config.MxClientIdProd,
+      password: config.MxApiSecretProd,
+      basePath: "https://api.mx.com",
+      vcEndpoint: "https://api.mx.com/",
+      aggregator: "mx",
+      available: true,
+    },
+  },
+  envConfig: {
+    HOSTURL: config.HOSTURL,
+  },
+});
+
 // This is where you add adapters
 export const adapterMap = {
+  ...mxAdapterMapObject,
   ...sophtronAdapterMapObject,
   ...testAdapterMapObject,
 };
