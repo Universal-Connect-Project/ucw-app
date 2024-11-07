cb048dfb9bc187f407c092f914b2078813e2cf02
Tyson Phalp <tyson.phalp@mx.com>
2024-11-07 15:25:14 -0700

    Put new code back in, along side original code

--- apps/server/src/connect/jobEndpoints.ts
+++ apps/server/src/connect/jobEndpoints.ts
@@ -7,7 +7,15 @@ export interface JobsRequest extends Request {
 }
 
 export const jobsRouteHandler = async (req: JobsRequest, res: Response) => {
-  if (["mx_int", "mx"].includes(req.connectApi.context?.aggregator)) {
+  if (
+    typeof req.connectApi.aggregatorAdapter?.RouteHandlers
+      ?.jobRequestHandler === "function"
+  ) {
+    await req.connectApi.aggregatorAdapter.RouteHandlers.jobRequestHandler(
+      req,
+      res,
+    );
+  } else if (["mx_int", "mx"].includes(req.connectApi.context?.aggregator)) {
     if (req.params.member_guid === "null") {
       res.send({ job: { guid: "none", job_type: WidgetJobTypes.AGGREGATION } });
       return;
