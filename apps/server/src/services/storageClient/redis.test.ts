70f9bd8be23d94d5f09ef32ded61080117068c58
Wes Risenmay <wes.risenmay@mx.com>
2024-11-13 07:28:13 -0700

    added a test for redis del

--- apps/server/src/services/storageClient/redis.test.ts
+++ apps/server/src/services/storageClient/redis.test.ts
@@ -1,76 +1,84 @@
-import preferences from '../../../cachedDefaults/preferences.json'
+import preferences from "../../../cachedDefaults/preferences.json";
 import {
   del as mockDel,
   sAdd as mockSAdd,
-  set as mockSet
-} from '../../__mocks__/redis'
-import config from '../../config'
-import { getPreferences } from '../../shared/preferences'
-import { get, getSet, overwriteSet, set, setNoExpiration } from './redis'
+  set as mockSet,
+} from "../../__mocks__/redis";
+import config from "../../config";
+import { getPreferences } from "../../shared/preferences";
+import { del, get, getSet, overwriteSet, set, setNoExpiration } from "./redis";
 
-describe('redis', () => {
-  it('loads the preferences into the cache after successful connection', async () => {
-    expect(await getPreferences()).toEqual(preferences)
-  })
+describe("redis", () => {
+  it("loads the preferences into the cache after successful connection", async () => {
+    expect(await getPreferences()).toEqual(preferences);
+  });
 
-  describe('get', () => {
-    it('gets a JSON.parsed value from the cache', async () => {
+  describe("get", () => {
+    it("gets a JSON.parsed value from the cache", async () => {
       const values = [
         false,
-        'testString',
+        "testString",
         { test: true },
         1234,
         null,
-        undefined
-      ]
-      const key = 'key'
+        undefined,
+      ];
+      const key = "key";
 
       for (const value of values) {
-        await set(key, value)
+        await set(key, value);
 
-        expect(await get(key)).toEqual(value)
+        expect(await get(key)).toEqual(value);
       }
-    })
-  })
+    });
+  });
 
-  describe('set', () => {
-    it('calls set on the client with EX by default', async () => {
-      await set('test', 'test')
+  describe("set", () => {
+    it("calls set on the client with EX by default", async () => {
+      await set("test", "test");
 
-      expect(mockSet).toHaveBeenCalledWith('test', JSON.stringify('test'), {
-        EX: config.RedisCacheTimeSeconds
-      })
-    })
+      expect(mockSet).toHaveBeenCalledWith("test", JSON.stringify("test"), {
+        EX: config.RedisCacheTimeSeconds,
+      });
+    });
 
-    it('calls set on the client with overriden parameters', async () => {
-      await set('test', 'test', {})
+    it("calls set on the client with overriden parameters", async () => {
+      await set("test", "test", {});
 
-      expect(mockSet).toHaveBeenCalledWith('test', JSON.stringify('test'), {})
-    })
-  })
+      expect(mockSet).toHaveBeenCalledWith("test", JSON.stringify("test"), {});
+    });
+  });
 
-  describe('overwriteSet', () => {
-    it('calls del on the client and then sAdd', async () => {
-      await overwriteSet('test', ['value1', 'value2'])
+  describe("del", () => {
+    it("calls del with the key", async () => {
+      await del("test");
 
-      expect(mockDel).toHaveBeenCalledWith('test')
-      expect(mockSAdd).toHaveBeenCalledWith('test', ['value1', 'value2'])
-    })
-  })
+      expect(mockDel).toHaveBeenCalledWith("test");
+    });
+  });
 
-  describe('getSet', () => {
-    it('calls del on the client and then sAdd', async () => {
-      const values = ['value1', 'value2']
-      await overwriteSet('test', values)
-      expect(await getSet('test')).toEqual(values)
-    })
-  })
+  describe("overwriteSet", () => {
+    it("calls del on the client and then sAdd", async () => {
+      await overwriteSet("test", ["value1", "value2"]);
 
-  describe('setNoExpiration', () => {
-    it('calls set on the client with no extra parameters', async () => {
-      await setNoExpiration('test', 'test')
+      expect(mockDel).toHaveBeenCalledWith("test");
+      expect(mockSAdd).toHaveBeenCalledWith("test", ["value1", "value2"]);
+    });
+  });
 
-      expect(mockSet).toHaveBeenCalledWith('test', JSON.stringify('test'), {})
-    })
-  })
-})
+  describe("getSet", () => {
+    it("calls del on the client and then sAdd", async () => {
+      const values = ["value1", "value2"];
+      await overwriteSet("test", values);
+      expect(await getSet("test")).toEqual(values);
+    });
+  });
+
+  describe("setNoExpiration", () => {
+    it("calls set on the client with no extra parameters", async () => {
+      await setNoExpiration("test", "test");
+
+      expect(mockSet).toHaveBeenCalledWith("test", JSON.stringify("test"), {});
+    });
+  });
+});
