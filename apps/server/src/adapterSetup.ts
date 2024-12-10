6ff347776d10739f4c64aa67dfa67ae521b2992e
Wes Risenmay <wes.risenmay@mx.com>
2024-12-10 10:23:34 -0700

    importing the mx and sophtron adapters.

--- apps/server/src/adapterSetup.ts
+++ apps/server/src/adapterSetup.ts
@@ -1,8 +1,47 @@
 import type { AdapterMap } from "@repo/utils";
 import { adapterMapObject as testAdapterMapObject } from "./test-adapter";
+import * as logger from "./infra/logger";
+import config from "./config";
+import { getMxAdapterMapObject } from "@ucp-npm/mx-adapter";
+import { getSophtronAdapterMapObject } from "@ucp-npm/sophtron-adapter";
+import { get, set } from "./services/storageClient/redis";
 
-// This is where you add adapters
+const mxAdapterMapObject = getMxAdapterMapObject({
+  cacheClient: {
+    set: set,
+    get: get,
+  },
+  logClient: logger,
+  aggregatorCredentials: {
+    mxInt: {
+      username: config.MX_CLIENT_ID,
+      password: config.MX_API_SECRET,
+    },
+    mxProd: {
+      username: config.MX_CLIENT_ID_PROD,
+      password: config.MX_API_SECRET_PROD,
+    },
+  },
+  envConfig: {
+    HOSTURL: config.HOST_URL,
+  },
+});
+
+const sophtronAdapterMapObject: Record<string, AdapterMap> =
+  getSophtronAdapterMapObject({
+    logClient: logger,
+    aggregatorCredentials: {
+      clientId: config.SOPHTRON_API_USER_ID,
+      secret: config.SOPHTRON_API_USER_SECRET,
+    },
+    envConfig: {
+      HOSTURL: config.HOST_URL,
+    },
+  });
+
 export const adapterMap: Record<string, AdapterMap> = {
+  ...mxAdapterMapObject,
+  ...sophtronAdapterMapObject,
   ...testAdapterMapObject,
 };
 
