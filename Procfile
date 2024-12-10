bc5efdda6720eee736f2b7f7b0bb22cb7c3b099e
Craig Tingey <craig.tingey@mx.com>
2024-12-10 10:59:29 -0700

    Fix things

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cp ./apps/server/cachedDefaults/stagingPreferences.json ./apps/server/cachedDefaults/preferences.json && npm start --workspace=apps/server
+web: npm run copyDemoPreferences && npm start --workspace=apps/server
--- apps/server/src/services/ElasticSearchClient.ts
+++ apps/server/src/services/ElasticSearchClient.ts
@@ -25,7 +25,7 @@ import { fetchInstitutions } from "./institutionSyncer";
 import { INSTITUTION_CURRENT_LIST_IDS } from "./storageClient/constants";
 import { getSet, overwriteSet } from "./storageClient/redis";
 
-const BATCH_SIZE = 25;
+const BATCH_SIZE = 50;
 
 export function getInstitutionFilePath() {
   return resolve(__dirname, "../../cachedDefaults/ucwInstitutionsMapping.json");
