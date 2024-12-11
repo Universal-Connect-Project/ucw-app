c6d2786f099faa4aad7546d79f816b8af6bad8b5
Tyson Phalp <tyson.phalp@mx.com>
2024-12-10 17:00:22 -0700

    Update readmes with delete user endpoint info

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -63,7 +63,9 @@ app.get("/health", function (req, res) {
 
 useUserEndpoints(app);
 useDataEndpoints(app);
+
 useAuthentication(app);
+
 useConnect(app);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
