d441ec02a30487e81ba2c0747fb1519d27c67d8b
Wes Risenmay <wes.risenmay@mx.com>
2025-01-16 11:55:11 -0700

    improve test name

--- apps/server/cypress/e2e/syncingSuite/institutionSyncing.cy.ts
+++ apps/server/cypress/e2e/syncingSuite/institutionSyncing.cy.ts
@@ -2,7 +2,7 @@ import { visitAgg } from "@repo/utils-dev-dependency";
 import { searchAndSelectTestExampleA } from "../../shared/utils/testExample";
 
 describe("institution syncing", () => {
-  it("shows a testExampleA institution even after the github setup nuked the institution mapping before starting the tests", () => {
+  it("shows a testExampleA institution even after the github setup nuked the institution mapping before starting the server", () => {
     visitAgg();
 
     searchAndSelectTestExampleA();
