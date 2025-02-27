872ba15f6e7517e02b2cab5d15fcaf975f31feb8
Wes Risenmay <wes.risenmay@mx.com>
2025-02-27 07:24:43 -0700

    added a test for updateMember

--- apps/ui/src/shared/test/testData/updateMFA.ts
+++ apps/ui/src/shared/test/testData/updateMFA.ts
@@ -1,12 +1,10 @@
 export const updateMFAResponse = {
-  member: {
-    institution_guid: "institutionCode",
-    guid: "testConnectionId",
-    connection_status: 0,
-    most_recent_job_guid: null,
-    is_oauth: false,
-    aggregator: "testExampleC",
-    is_being_aggregated: false,
-    mfa: {},
-  },
+  institution_guid: "institutionCode",
+  guid: "testConnectionId",
+  connection_status: 0,
+  most_recent_job_guid: null,
+  is_oauth: false,
+  aggregator: "testExampleC",
+  is_being_aggregated: false,
+  mfa: {},
 };
