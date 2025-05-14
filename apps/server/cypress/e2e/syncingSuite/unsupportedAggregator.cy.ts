df7fb3a50f0d6a751d99665b1a66d0c1e469879a
Wes Risenmay <wes.risenmay@mx.com>
2025-05-14 15:30:53 -0600

    attempting to fix e2e tests

--- apps/server/cypress/e2e/syncingSuite/unsupportedAggregator.cy.ts
+++ apps/server/cypress/e2e/syncingSuite/unsupportedAggregator.cy.ts
@@ -3,9 +3,9 @@ import { searchByText, visitAgg } from "@repo/utils-cypress";
 describe("unsupported aggregator", () => {
   it("filters out institutions which are not supported by an aggregator in your list of supported aggregators.", () => {
     visitAgg();
-    searchByText("MX Bank");
+    searchByText("sophtron");
 
-    cy.findByText("MWABank").should("exist");
-    cy.findByText("MX Bank").should("not.exist");
+    cy.findByText("Soperton").should("exist");
+    cy.findByText("Sophtron Bank").should("not.exist");
   });
 });
