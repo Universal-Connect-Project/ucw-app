2e44c30aad999e7664b5ae65d9ef2c05d1640030
Wes Risenmay <wes.risenmay@mx.com>
2025-05-15 11:45:09 -0600

    fixed getMembers test

--- apps/server/cypress/e2e/suite1/endpoints/addMember.cy.ts
+++ apps/server/cypress/e2e/suite1/endpoints/addMember.cy.ts
@@ -1,39 +1,11 @@
-import { ComboJobTypes, MEMBERS_URL } from "@repo/utils";
-import { MX_INT_AGGREGATOR_STRING } from "@repo/mx-adapter";
+import { addMember } from "../../../shared/utils/mx";
 
 describe("addMember", () => {
   it("responds with a member", () => {
-    const userId = Cypress.env("userId");
-
-    cy.request({
-      body: {
-        institution_guid: "mxbank",
-        credentials: [
-          {
-            guid: "CRD-9f61fb4c-912c-bd1e-b175-ccc7f0275cc1",
-            value: "mxuser",
-          },
-          {
-            guid: "CRD-e3d7ea81-aac7-05e9-fbdd-4b493c6e474d",
-            value: "correct",
-          },
-        ],
-      },
-      headers: {
-        meta: JSON.stringify({
-          aggregator: MX_INT_AGGREGATOR_STRING,
-          jobTypes: [ComboJobTypes.TRANSACTIONS],
-          userId,
-        }),
-      },
-      method: "POST",
-      url: MEMBERS_URL,
-    }).then(
+    addMember().then(
       (
         response: Cypress.Response<{
-          member: {
-            institution_guid: string;
-          };
+          member: {};
         }>,
       ) => {
         expect(response.status).to.eq(200);
@@ -43,7 +15,17 @@ describe("addMember", () => {
 
         expect(member).to.exist;
 
-        expect(member.institution_guid).to.exist;
+        const expectedProperties = [
+          "aggregator",
+          "connection_status",
+          "guid",
+          "institution_guid",
+          "is_oauth",
+        ];
+
+        expectedProperties.forEach((property) => {
+          expect(member[property]).to.exist;
+        });
       },
     );
   });
