cd98e9a588fd2c4886c97844adcbefc9d98bd0ef
Wes Risenmay <wes.risenmay@mx.com>
2025-05-16 13:32:12 -0600

    fix search e2e tests

--- apps/server/cypress/shared/constants/testExample.ts
+++
@@ -1,2 +0,0 @@
-export const TEST_EXAMPLE_A_ONLY_INSTITUTION_NAME = 'TestExampleA Bank'
-export const TEST_EXAMPLE_B_ONLY_INSTITUTION_NAME = 'TestExampleB Bank'
--- packages/mx-adapter/src/testInstitutions.ts
+++ packages/mx-adapter/src/testInstitutions.ts
@@ -3,8 +3,29 @@ import type { CachedInstitution } from "@repo/utils";
 export const MX_BANK_UCP_INSTITUTION_ID = "mxBank";
 export const MX_BANK_TO_HIDE_NAME = "MX Bank to hide";
 export const MX_BANK_NAME = "MX Bank";
+export const MX_BANK_TRANSACTIONS_ONLY_NAME = "MX Bank transactions only";
+export const MX_BANK_ROUTING_NUMBER = "111111111";
 
 export const testInstitutions: CachedInstitution[] = [
+  {
+    name: MX_BANK_TRANSACTIONS_ONLY_NAME,
+    id: "mxBankTransactionsOnly",
+    keywords: ["Gringotts", "MX Bank"],
+    logo: "https://content.moneydesktop.com/storage/MD_Assets/Ipad%20Logos/100x100/INS-3aeb38da-26e4-3818-e0fa-673315ab7754_100x100.png",
+    url: "https://mx.com",
+    is_test_bank: true,
+    routing_numbers: [],
+    mx: {
+      id: "mxbank",
+      supports_aggregation: true,
+      supports_history: false,
+      supports_identification: false,
+      supports_oauth: false,
+      supports_verification: false,
+      supportsRewards: false,
+      supportsBalance: false,
+    },
+  },
   {
     name: MX_BANK_TO_HIDE_NAME,
     id: "mxBankToHide",
@@ -31,7 +52,7 @@ export const testInstitutions: CachedInstitution[] = [
     logo: "https://content.moneydesktop.com/storage/MD_Assets/Ipad%20Logos/100x100/INS-3aeb38da-26e4-3818-e0fa-673315ab7754_100x100.png",
     url: "https://mx.com",
     is_test_bank: true,
-    routing_numbers: [],
+    routing_numbers: [MX_BANK_ROUTING_NUMBER],
     mx: {
       id: "mxbank",
       supports_aggregation: true,
