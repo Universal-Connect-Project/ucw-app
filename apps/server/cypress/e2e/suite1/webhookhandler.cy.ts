6489fbc4c54e31f291eb0dffac0f460aaa4f9d43
Wes Risenmay <wes.risenmay@mx.com>
2025-05-16 13:39:48 -0600

    fix webhook handler tests

--- apps/server/cypress/e2e/suite1/webhookhandler.cy.ts
+++ apps/server/cypress/e2e/suite1/webhookhandler.cy.ts
@@ -1,13 +1,15 @@
+import { MX_INT_AGGREGATOR_STRING } from "@repo/mx-adapter";
+import { ConnectionStatus } from "@repo/utils";
+
 describe("webhook", () => {
   const PORT: number = 8080;
 
-  it("receives the request without errors", async () => {
-    cy.request(`http://localhost:${PORT}/webhook/testExampleA/?code=success_code&state=request_id`).then(
-      (response: Cypress.Response<{ message: string }>) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.eq("");
-      },
-    );
+  it("receives the request without errors", () => {
+    cy.request({
+      url: `http://localhost:${PORT}/webhook/${MX_INT_AGGREGATOR_STRING}/?status=success`,
+    }).then((response: Cypress.Response<{ status: number }>) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.status).to.eq(ConnectionStatus.CONNECTED);
+    });
   });
-
 });
