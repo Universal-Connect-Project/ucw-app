3e4c38029b2b9e40a247f5026f02ba6c1c4c6e15
Wes Risenmay <wes.risenmay@mx.com>
2025-05-16 07:18:07 -0600

    fix more tests

--- apps/server/cypress/e2e/suite1/endpoints/memberCredentials.cy.ts
+++ apps/server/cypress/e2e/suite1/endpoints/memberCredentials.cy.ts
@@ -1,35 +1,43 @@
-import { TEST_EXAMPLE_A_AGGREGATOR_STRING } from "../../../../src/test-adapter/constants";
+import { MX_INT_AGGREGATOR_STRING } from "@repo/mx-adapter";
+import { addMember } from "../../../shared/utils/mx";
 
 describe("member credentials", () => {
   it("returns a set of credentials", () => {
-    const testConnectionId = "testExampleA";
+    addMember().then((response) => {
+      const connectionId = response.body.member.guid;
 
-    cy.request({
-      headers: {
-        meta: JSON.stringify({
-          aggregator: TEST_EXAMPLE_A_AGGREGATOR_STRING,
-        }),
-      },
-      url: `/members/${testConnectionId}/credentials`,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
+      const resolvedUserId = JSON.parse(
+        response.headers.meta as string,
+      ).resolvedUserId;
 
-      const { body } = response;
+      cy.request({
+        headers: {
+          meta: JSON.stringify({
+            aggregator: MX_INT_AGGREGATOR_STRING,
+            resolvedUserId,
+          }),
+        },
+        url: `/members/${connectionId}/credentials`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
 
-      expect(body.length).to.be.greaterThan(0);
+        const { body } = response;
 
-      const [firstResult] = body;
+        expect(body.length).to.be.greaterThan(0);
 
-      const expectedProperties = [
-        "id",
-        "label",
-        "guid",
-        "field_name",
-        "field_type",
-      ];
+        const [firstResult] = body;
 
-      expectedProperties.forEach((property) => {
-        expect(firstResult[property]).to.exist;
+        const expectedProperties = [
+          "id",
+          "label",
+          "guid",
+          "field_name",
+          "field_type",
+        ];
+
+        expectedProperties.forEach((property) => {
+          expect(firstResult[property]).to.exist;
+        });
       });
     });
   });
