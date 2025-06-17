ccdb5976d81a675a06164d13abaea7f6add58559
Craig Tingey <tingeycraig@gmail.com>
2025-06-17 11:39:29 -0600

    remove double prod env variables

--- apps/server/src/dotEnv.ts
+++ apps/server/src/dotEnv.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
-if (process.env.PRODUCTION === "true") {
+if (process.env.ENV === "prod") {
   dotenv.config({
     path: [
       path.join(__dirname, "../env/production.env"),
