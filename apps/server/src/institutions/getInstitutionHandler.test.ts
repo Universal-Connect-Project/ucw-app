008efe881c9d6d07a87ced6002c18440cb20b4a9
Wes Risenmay <wes.risenmay@mx.com>
2025-09-02 08:37:53 -0600

    added a test for getInstitutionHandler and some skeletons for other required tests

--- apps/server/src/institutions/getInstitutionHandler.test.ts
+++ apps/server/src/institutions/getInstitutionHandler.test.ts
@@ -8,13 +8,86 @@ import {
   MX_AGGREGATOR_STRING,
   MX_INT_AGGREGATOR_STRING,
 } from "@repo/mx-adapter";
-import { mxTestData } from "@repo/utils-dev-dependency";
+import { mxTestData, waitFor } from "@repo/utils-dev-dependency";
+import setupPerformanceHandlers from "../shared/test/setupPerformanceHandlers";
 
 const { institutionData: mxInstitutionData } = mxTestData;
 
 const ucpInstitutionId = "testAggregatorInstitutionGuid";
 
 describe("getInstitutionHandler", () => {
+  describe("should record performance", () => {
+    describe("success", () => {
+      it("sets a new performanceSessionId on context, records a start event with the correct props, and records a pause event with the correct props", async () => {
+        const connectionId = crypto.randomUUID();
+
+        jest.spyOn(crypto, "randomUUID").mockReturnValueOnce(connectionId);
+
+        const req = {
+          context: {
+            jobTypes: [ComboJobTypes.TRANSACTIONS],
+          },
+          params: {
+            institution_guid: ucpInstitutionId,
+          },
+        } as unknown as Request;
+
+        const res = {
+          send: jest.fn(),
+        } as unknown as Response;
+
+        const requestLog = setupPerformanceHandlers([
+          "connectionStart",
+          "connectionPause",
+        ]);
+
+        await getInstitutionHandler(req, res);
+
+        expect(requestLog[0]).toEqual(
+          expect.objectContaining({
+            method: "POST",
+            eventType: "connectionStart",
+            connectionId,
+            body: {
+              aggregatorId: MX_AGGREGATOR_STRING,
+              institutionId: "testAggregatorInstitutionGuid",
+              jobTypes: req.context.jobTypes,
+              recordDuration: true,
+              shouldRecordResult: false,
+            },
+          }),
+        );
+
+        await waitFor(() => {
+          expect(requestLog.length).toBe(2);
+        });
+
+        expect(requestLog[1]).toEqual(
+          expect.objectContaining({
+            method: "PUT",
+            eventType: "connectionPause",
+            connectionId,
+            body: {},
+          }),
+        );
+      });
+    });
+
+    describe("failure", () => {
+      it("sets a new performanceSessionId on context, records a start event with the correct props, records a pause event with shouldRecordResult if the request fails, and responds with a 400", () => {});
+    });
+  });
+
+  describe("shouldn't record performance", () => {
+    describe("success", () => {
+      it("sets a new performanceSessionId on context and doesn't record a start or pause event", () => {});
+    });
+
+    describe("failure", () => {
+      it("sets a new performanceSessionId on context, doesn't record start or pause events if the request fails, and responds with a 400", () => {});
+    });
+  });
+
   it("returns an institution object with name, url, and logo_url properties from the ucp institution list", async () => {
     jest.spyOn(preferences, "getPreferences").mockResolvedValue({
       ...testPreferences,
